e7f5e65709e26174b931c0f838a2aa74f6ea04a1
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2023-12-17 15:18:24 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.markdown("# Gregory S Davis")
+st.markdown("## What I'm working on....")
+st.markdown("""
+    I've created a few small Data Science, Python, R and related projects. 
+    
+    Find me on Google Scholar
+    [page](https://scholar.google.com/citations?user=DB6vA7gAAAAJ&hl=en).    
+    
+    Check out the named entity extraction tool [here](http://website-97957.appspot.com/)
+    
+    
+    
+    
+""")
+
+with st.container():
+    image_col, text_col = st.columns((1,2))
+    with image_col:
+        st.image("Relaxing.jpg")
+
+    with text_col:
+        st.subheader("ChatGPT")
+        st.write("""Ask the chatbot questions and interact with OpenAI's ChatGPT
+            """)
+        st.markdown("[here](https://covid-19-001.herokuapp.com/)")
+
