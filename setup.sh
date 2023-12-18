8612e34409190e3026ff37b969773bdbd10bc676
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2023-12-17 18:30:10 -0600

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,17 +4,3 @@ echo "\
 [general]\n\
 email = \"gregory_s_davis@yahoo.com\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-\n\
-[theme]\n\
-primaryColor = '#eb4034'\n\
-backgroundColor = '#021d24'\n\
-secondaryBackgroundColor = '#B9F1C0'\n\
-textColor = '#FFFFFF'\n\
-font = 'sans serif'\n\
-" > ~/.streamlit/config.toml
