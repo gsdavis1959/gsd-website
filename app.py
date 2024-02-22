ed0da6ab25e3c991bd4a4feb54a718cabfe7f9a7
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2024-02-22 16:36:32 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 import os
 import streamlit as st
 
-st.markdown("# Gregory S Davis")
-st.markdown("## What I'm working on....")
-st.markdown("""
-    I've created a few small Data Science, Python, R and related projects. 
-    
-    Find me on Google Scholar
-    [page](https://scholar.google.com/citations?user=DB6vA7gAAAAJ&hl=en).    
-    
-    Check out the named entity extraction tool [here](http://website-97957.appspot.com/)
-    
-    
-    
-    
-""")
-
 with st.container():
     image_col, text_col = st.columns((1,2))
     with image_col:
         st.image("Relaxing.jpg")
+st.markdown("# Gregory S Davis")
+st.markdown("## What I'm working on....")
+st.markdown("""
+Here are a few ideas I've been working on: 
+      
+Check out the named entity extraction tool 
+    [here](http://website-97957.appspot.com/)
+    
+Try out Google's Gemini Large Language Model (LLM) 
+    [here](https://gsd-gemini-2e8b5ea02d39.herokuapp.com/)
 
-    with text_col:
-        st.subheader("ChatGPT")
-        st.write("""Ask the chatbot questions and interact with OpenAI's ChatGPT
-            """)
-        st.markdown("[here](https://covid-19-001.herokuapp.com/)")
+Ask Open AI to analyze a graph or image [here](https://gsd-graph-analysis-d87538823918.herokuapp.com/)
+
+Chatbot written by Open AI's ChatGPT [here](https://covid-19-001.herokuapp.com/)
+
+If you need a bad joke, try this using Open AI, Langchain, and Dash [here](https://gsd-joke-abd69206d90b.herokuapp.com/)
+
+    
+  
+    
+""")
+
+
+
+
 
